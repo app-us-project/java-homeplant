f3973db2164015aecfb10248cd256a535ee09ff0
icarus8050 <icarus8050@naver.com>
2021-02-01 23:23:23 +0900

    health check controller

--- src/main/java/com/appus/homeplant/repository/FooRepository.java
+++
@@ -1,7 +0,0 @@
-package com.appus.homeplant.repository;
-
-import com.appus.homeplant.core.Foo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FooRepository extends JpaRepository<Foo, Long> {
-}
