47da5f23ab8b5c5a2d82e53d209c8bd9b25adb09
sujin7837 <sujin7837@naver.com>
2021-02-03 00:16:05 +0900

    update_commit

--- src/main/java/com/appus/homeplant/service/AccountService.java
+++ src/main/java/com/appus/homeplant/service/AccountService.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
+
     private final FooRepository fooRepository;
     private final HttpSession httpSession;
 
