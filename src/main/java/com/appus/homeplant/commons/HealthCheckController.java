ce4485cbcd7f16d95c2d6eaf05be58d26a1da670
icarus8050 <icarus8050@naver.com>
2021-02-18 00:01:49 +0900

    health check

---
+++ src/main/java/com/appus/homeplant/commons/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.appus.homeplant.commons;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String check() {
+        return "health";
+    }
+}
